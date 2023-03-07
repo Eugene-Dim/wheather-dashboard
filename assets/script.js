04f29d62e8844d38f94c76069ee840be2223a783
Eugene Dimov <eugenedostoevsky@Eugenes-MBP.MG8702>
2023-03-06 21:13:19 -0800

    .

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 var apiKey = '430e82179b15a482d85701e3ea9faa3e';
 var dateEl = $('#date');
 var cityEl = $('#city');
-var currentTempEl = $('#currentTemp');
-var currentFeelsEl = $('#currentFeels');
-var currentHumidityEl = $('#currentHumidity');
-var currentWindEl = $('#currentWind');
-var weatherIconEl = $('#weatherIcon');
+var currentTempEl = $('#temperature');
+var currentFeelsEl = $('#feels');
+var currentHumidityEl = $('#humidity');
+var currentWindEl = $('#windy');
+var weatherIconEl = $('#Icon');
 var searchHistoryEl = $('.searchHistory')
 var searchHistArray = JSON.parse(localStorage.getItem('searchHistArray'))||[];
 var api = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
