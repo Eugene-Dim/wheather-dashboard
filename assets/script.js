34b7d3cef58fda143c4061525a429df7f0a832ad
eugene-dim <dimovjenea@gmail.com>
2023-03-15 20:13:25 -0700

    chnaged javascript

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,8 @@
-  var currentWeather = document.querySelector('#currentWeather')
-  var searchButton = document.querySelector('#search-btn')
-  var fiveDayEl = document.querySelector('#fiveDay')
-  var windEl = document.querySelector('#wind')
-  var humidityEl = document.querySelector('#humidity')
-
-  var searchHistory = [];
+var searchHistory = [];
   var weatherApiRootUrl = 'https://api.openweathermap.org';
   var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
   
-  // DOM element references
+ 
   var searchForm = document.querySelector('#search-form') ;
   var searchInput = document.querySelector('#search-input') ;
   var todayContainer = document.querySelector('#today') ;
@@ -18,110 +12,89 @@
   // Api Key
   var apiKey = `a9854075d238a8c3e83e544ff046d4c4`
 
-  
-  // Function to display the search history list.
-  function renderSearchHistory() {
-    searchHistoryContainer.innerHTML = '';
-  
-    // Start at end of history array and count down to show the most recent at the top.
-    for (var i = searchHistory.length - 1; i >= 0; i--) {
-      var btn = document.createElement('button');
-      btn.setAttribute('type', 'button');
-      btn.setAttribute('aria-controls', 'today forecast');
-      btn.classList.add('history-btn', 'btn-history');
-  
-      // `data-search` allows access to city name when click handler is invoked
-      btn.setAttribute('data-search', searchHistory[i]);
-      btn.textContent = searchHistory[i];
-      searchHistoryContainer.append(btn);
-    }
-  }
-  
-  // Function to update history in local storage then updates displayed history.
-  function appendToHistory(search) {
-    // If there is no search term return the function
-    if (searchHistory.indexOf(search) !== -1) {
-      return;
-    }
-    searchHistory.push(search);
-  
-    localStorage.setItem('search-history', JSON.stringify(searchHistory));
-    renderSearchHistory();
-  }
-  
-  // Function to get search history from local storage
-  function initSearchHistory() {
-    var storedHistory = localStorage.getItem('search-history');
-    if (storedHistory) {
-      searchHistory = JSON.parse(storedHistory);
-    }
-    renderSearchHistory();
-  }
 
+function renderSearchHistory() {
+  searchHistoryContainer.innerHTML = '';
 
-// Function to display 5 day forecast.
+  for (var i = searchHistory.length - 1; i >= 0; i--) {
+    var btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('aria-controls', 'today forecast');
+    btn.classList.add('history-btn', 'btn-history');
+
+    
+    btn.setAttribute('data-search', searchHistory[i]);
+    btn.textContent = searchHistory[i];
+    searchHistoryContainer.append(btn);
+  }
+}
+
+function appendToHistory(search) {
+  
+  if (searchHistory.indexOf(search) !== -1) {
+    return;
+  }
+  searchHistory.push(search);
+
+  localStorage.setItem('search-history', JSON.stringify(searchHistory));
+  renderSearchHistory();
+}
+
+function initSearchHistory() {
+  var storedHistory = localStorage.getItem('search-history');
+  if (storedHistory) {
+    searchHistory = JSON.parse(storedHistory);
+  }
+  renderSearchHistory();
+}
+
 function renderForecast(dailyForecast) {
-    // Create unix timestamps for start and end of 5 day forecast
-    var startDt = dayjs().add(1, 'day').startOf('day').unix();
-    var endDt = dayjs().add(6, 'day').startOf('day').unix();
   
-    var headingCol = document.createElement('div');
-    var heading = document.createElement('h4');
-  
-    headingCol.setAttribute('class', 'col-12');
-    heading.textContent = '5-Day Forecast:';
-    headingCol.append(heading);
+  var startDt = dayjs().add(1, 'day').startOf('day').unix();
+  var endDt = dayjs().add(6, 'day').startOf('day').unix();
 
+  var headingCol = document.createElement('div');
+  var heading = document.createElement('h4');
+
+  headingCol.setAttribute('class', 'col-12');
+  heading.textContent = '5-Day Forecast:';
+  headingCol.append(heading);
 }
 
 function renderItems(city, data) {
-    renderCurrentWeather(city, data.list[0], data.city.timezone);
-    renderForecast(data.list);
-  }
+  renderCurrentWeather(city, data.list[0], data.city.timezone);
+  renderForecast(data.list);
+}
 
-  // Fetches weather data for given location from the Weather Geolocation
-// endpoint; then, calls functions to display current and forecast weather data.
 function fetchWeather(location) {
-    var { lat } = location;
-    var { lon } = location;
-    var city = location.name;
-  
-    var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
-  
-    fetch(apiUrl)
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (data) {
-        renderItems(city, data);
-      })
-      .catch(function (err) {
-        console.error(err);
-      });
-  }
-  
-  function fetchCoords(search) {
-    var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
-  
-    fetch(apiUrl)
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (data) {
-        if (!data[0]) {
-          alert('Location not found');
-        } else {
-          appendToHistory(search);
-          fetchWeather(data[0]);
-        }
-      })
-      .catch(function (err) {
-        console.error(err);
-      });
-  }
+  var { lat, lon, name: city } = location;
 
-  function handleSearchFormSubmit(e) {
-  // Don't continue if there is nothing in the search form
+  var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
+
+  fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => renderItems(city, data))
+    .catch(err => console.error(err));
+}
+
+function fetchCoords(search) {
+  var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
+
+  fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => {
+      if (!data[0]) {
+        alert('Location not found');
+      } else {
+        appendToHistory(search);
+        fetchWeather(data[0]);
+      }
+    })
+    .catch(err => console.error(err));
+}
+
+function handleSearchFormSubmit(e) {
+  
   if (!searchInput.value) {
     return;
   }
@@ -133,7 +106,7 @@ function fetchWeather(location) {
 }
 
 function handleSearchHistoryClick(e) {
-  // Don't do search if current elements is not a search history button
+  
   if (!e.target.matches('.btn-history')) {
     return;
   }
@@ -143,9 +116,6 @@ function handleSearchHistoryClick(e) {
   fetchCoords(search);
 }
 
-  initSearchHistory();
+initSearchHistory();
 searchForm.addEventListener('submit', handleSearchFormSubmit);
 searchHistoryContainer.addEventListener('click', handleSearchHistoryClick);
-
-
-
